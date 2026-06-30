9911d6b87f582d26d7223fd8c32b72c2c65972b8
smrutich <41258197+smrutich@users.noreply.github.com>
2026-06-29 23:59:35 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashT = {}
+
+        for s in strs:
+            append = False
+            count = self.counter(s)
+            for k, v in hashT.items():
+                if v[0] == count:
+                    v[1].append(s)
+                    append = True
+                    break
+            if append == False:
+                hashT[s] = (count,[s])
+            # print(hashT)
+        res = []
+        for v in hashT.values():
+            res.append(v[1])
+        return res
+
+        # hashT = {}
+
+        # for s in strs:
+        #     check = ''.join(sorted(s))
+        #     if check in hashT:
+        #         hashT[check].append(s)
+        #     else:
+        #         hashT[check] = [s]
+        # res = []
+        # for v in hashT.values():
+        #     res.append(v)
+        # return res
+            
+
+    def counter(self,s:str):
+        c = {}
+        for char in s:
+            c[char] = c.get(char,0) + 1
+        return c
+        
